75de9410f4636e5441725f2746687d5862160076
Daria <nabatova.darya@protonmail.com>
2020-04-02 02:02:20 +0300

    surf section desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,20 @@ $(function () {
         asNavFor: '.header-slider'
     });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__prev" src="images/previous.svg" width="24" height="20" alt="Previous">',
+        nextArrow: '<img class="slider-arrows slider-arrows__next" src="images/next.svg" width="24" height="20" alt="Next">',
+        asNavFor: '.surf-map'
+    });
+
+    $('.surf-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
+    })
+
 });
